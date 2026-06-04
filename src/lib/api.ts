41a7ba14f8166def993c93e0cef49f61047123f9
naufalfad <naufalfad29@gmail.com>
2026-06-04 16:13:28 +0700

    .env perbaiki

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 // src/lib/api.ts
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: API_URL,
